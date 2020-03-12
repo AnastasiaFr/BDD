068dd229fb107982bb476fb46b96daa05e352aff
frolova-a <Anastasiya.Frolova@bercut.com>
2020-03-12 12:59:56 +0300

    add new test

--- src/test/java/com/tests/YearStepDefinition.java
+++ src/test/java/com/tests/YearStepDefinition.java
@@ -11,7 +11,7 @@ class LeapYear {
         if ((year % 400 == 0) || ((year % 4 == 0))) {
             return "Високосный";
         } else {
-            return "Не вискосный";
+            return "Не високосный";
         }
     }
 }
